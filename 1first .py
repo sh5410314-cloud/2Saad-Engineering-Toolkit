21b2967293c038c93abfc1e5e767a5e6252900bb
sh5410314-cloud <sh5410314@gmail.com>
2026-06-16 16:12:37 +0600

    Create 1first .py

---
+++ 1first .py
@@ -0,0 +1,4 @@
+print("CEO Saad এর খাতা = itel vision1 CEO")
+print("CEO Saad এর কলম = আঙ্গুল CEO") 
+print("CEO Saad এর মটো = No Compromise Only Run CEO!")
+print("2SAD = জিদ + জয় + ইনশাআল্লাহ CEO!")
